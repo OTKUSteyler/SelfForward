556b356db4f7ceb192599be6135e56327198c454
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2026-02-01 21:03:37 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 export default {
     name: "SelfForward",
-    description: "Adds the current channel to the forward list popup",
+    description: "Adds the current channel and self DM to the forward list popup",
     authors: ["VillainsRule"],
     patches: [
         {
             find: ".getChannelHistory(),",
-            replacement: [{
-                match: /\i.id\]/,
-                replace: "]"
-            }]
+            replacement: [
+                {
+                    // Remove filter that excludes current channel
+                    match: /(\i)\.filter\(\i=>\i\.id!==(\i)\.id\)/,
+                    replace: "$1"
+                },
+                {
+                    // Remove filter that excludes self DM
+                    match: /\.filter\(\i=>\i\.getRecipientId\(\)!==(\i)\.id\)/,
+                    replace: ""
+                }
+            ]
         }
     ],
     onLoad: () => {
